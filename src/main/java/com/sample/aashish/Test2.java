00192e36332739bec1b1d7e1f9e6d5d3c98e4df4
sthasis100 <aashishshrestha540@gmail.com>
2022-03-06 19:14:13 -0500

    Second commit

---
+++ src/main/java/com/sample/aashish/Test2.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.sample.aashish;
+
+/**
+ * @author aashishshrestha
+ *
+ */
+public class Test2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
