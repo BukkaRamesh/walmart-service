a23c6f2cdc57f4a882fc40db09c06f3f609da0ac
41879 <sriyog@gmail.com>
2022-03-09 05:27:46 -0500

    assignment 2

---
+++ src/main/java/com/sample/sridevi/Math.java
@@ -0,0 +1,66 @@
+package com.sample.sridevi;
+
+public class Math {
+	public static  int a=3;
+	public int b=5;
+	public  int c=3;
+	public int d=4;
+	
+	
+	
+	
+	
+		
+	public static void main(String[] args) {
+		int b=a;
+		int c=a;
+		
+		
+		
+		
+		
+		System.out.println(a);
+		
+			System.out.println(a==b);
+			System.out.println(a!=b);
+			System.out.println(a>b);
+			System.out.println(a<b);
+			System.out.println(a<b);
+			System.out.println(a<=b);
+			System.out.println(a>=b);
+			
+			for(int i=0;i<=5;i++) {
+				System.out.println(i);
+				
+				for (int a=3;a<=5;a++) {
+					System.out.println(a);
+							
+					
+					for (int s=3;s>=0;s--);
+					System.out.println(a);
+					
+					String name = "sam";
+					String address="US";
+					
+					if(name == address);{
+					System.out.println("True");
+				
+					if ((a<b) && (c<b));
+					System.out.println("True");
+	
+				}
+					
+					
+					
+					
+					
+					
+					
+					
+					
+				}
+			}
+	}
+			
+
+}
