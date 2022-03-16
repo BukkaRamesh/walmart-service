0f7982c8fa4a237083c208be6b6f3d3275304a78
Ze Be Zabin <zzabin81@gmail.com>
2022-03-16 11:29:21 -0400

    Initial Commit

---
+++ src/main/java/com/sample/zabin/Test1.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.sample.zabin;
+
+/**
+ * @author zabin
+ *
+ */
+public class Test1 {
+	public static void main(String[] args) {
+		System.out.println("Hi");
+		
+	}
+
+}
