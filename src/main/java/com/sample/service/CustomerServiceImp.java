9e00dbb446dd6280c40ea52a6fd0b60558f55e3e
sthasis100 <aashishshrestha540@gmail.com>
2022-03-23 19:46:04 -0400

    Put, Delete & checkink duplicates added

--- src/main/java/com/sample/service/CustomerServiceImp.java
+++ src/main/java/com/sample/service/CustomerServiceImp.java
@@ -1,9 +1,12 @@
 package com.sample.service;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,24 +22,47 @@ public class CustomerServiceImp implements CustomerService {
 
 	@Override
 	public void saveCustomer(Customer customer) {
-		Customer dbCustomerObject = customerRepo.getById(customer.getCustomerId());
-		if (dbCustomerObject.getCustomerId().equals(customer.getCustomerId())) {
-			System.out.println("Customer already existed");
-		} else {
-			customerRepo.save(customer);
-			System.out.println("Customers saved successfully");
-		}
+//		Customer dbCustomerObject = customerRepo.getById(customer.getCustomerId());
+//		if (dbCustomerObject.getCustomerId().equals(customer.getCustomerId())) {
+//			System.out.println("Customer already existed");
+//		} else {
+//			customerRepo.save(customer);
+//			System.out.println("Customers saved successfully");
+//		}
+		customerRepo.save(customer);
 
 	}
 
 	@Override
 	public List<Customer> getCustomer() {
-		List<Customer> getCustomer = new ArrayList<Customer>();
-		getCustomer.addAll(customerRepo.findAll());
+		List<Customer> getCustomer = customerRepo.findAll();
+		List<Customer> finalList = new ArrayList<Customer>();
 
-		Set<Customer> validCustomer = new HashSet<Customer>(getCustomer);
+		boolean found = false;
+		// checking whether the list contains any duplicates
+		for (Customer cust : getCustomer) {
+			for (Customer cust1 : finalList) {
+				if (cust.getCustomerNumber().equals(cust1.getCustomerNumber())) {
+					found = true;
+					break;
+				}
+			}
+			if (!found)
+				finalList.add(cust);
+		}
 
-		return (List<Customer>) validCustomer;
+//		List<Customer> getCustomer2 = customerRepo.findAll();
+//		Set<Customer> cus = new HashSet<>();
+//		Set<Customer> uniqueRecords = getCustomer2.stream().filter(n->!cus.add(n)).collect(Collectors.toSet());
+
+		List<Customer> getCustomer2 = customerRepo.findAll();
+		Set<Customer> finalSet = getCustomer2.stream()
+				.collect(Collectors.toCollection(() -> new TreeSet<>(Comparator.comparing(Customer::getCustomerName))));
+
+		List<Customer> listIsSorted = finalSet.stream().collect(Collectors.toList());
+
+		return listIsSorted;
+
 	}
 
 	@Override
@@ -53,8 +79,7 @@ public class CustomerServiceImp implements CustomerService {
 
 	@Override
 	public void deleteCustomerById(Long customerId) {
-		Customer cos = customerRepo.getOne(customerId);
-		customerRepo.delete(cos);
+		customerRepo.deleteById(customerId);
 
 	}
 
